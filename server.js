399cc14c4f6dbb0bd94ec5bb95c425150765df13
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-09-05 16:17:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-// /backend/server.js
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
 
-// Importar as rotas
-const feliz1Routes = require('./routes/feliz1Routes');
-const papelariaRoutes = require('./routes/papelariaRoutes');
-const reuniaoRoutes = require('./routes/reuniaoRoutes');
+// Importar os routers
+const feliz1Routes = require('./feliz1/router/feliz1');
+const papelariaRoutes = require('./feliz1/router/papelaria');
+const reuniaoRoutes = require('./feliz1/router/reuniao');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Rotas
+// Definir as rotas
 app.use('/feliz1', feliz1Routes);
 app.use('/papelaria', papelariaRoutes);
 app.use('/reuniao', reuniaoRoutes);
 
-// Servir arquivos estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, '../Reuniao-main/public')));
-app.use(express.static(path.join(__dirname, '../feliz1')));
-app.use(express.static(path.join(__dirname, '../Papelaria')));
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'feliz1')));
+app.use(express.static(path.join(__dirname, 'Papelaria')));
+app.use(express.static(path.join(__dirname, 'Reuniao-main/public')));
 
-// Iniciar o servidor
+// Inicializar o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
