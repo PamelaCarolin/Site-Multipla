69a689e3dce7072b5d8f1c82fd826d5feb5e40f8
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-09-06 08:12:12 -0300

    Delete router directory

--- router/papelaria.js
+++
@@ -1,9 +0,0 @@
-// /backend/routes/papelariaRoutes.js
-const express = require('express');
-const router = express.Router();
-const papelariaController = require('../controllers/papelariaController');
-
-router.get('/products', papelariaController.getProducts);
-router.post('/cart', papelariaController.addToCart);
-
-module.exports = router;
--- router/reuniao.js
+++
@@ -1,8 +0,0 @@
-// /backend/routes/reuniaoRoutes.js
-const express = require('express');
-const router = express.Router();
-const reuniaoController = require('../controllers/reuniaoController');
-
-router.post('/schedule', reuniaoController.scheduleMeeting);
-
-module.exports = router;
