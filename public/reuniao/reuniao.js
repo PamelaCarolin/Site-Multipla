739a2e3d462de3217e83664acaaf1b61a807168e
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-09-06 10:49:37 -0300

    Update reuniao.js

--- public/reuniao/reuniao.js
+++ public/reuniao/reuniao.js
@@ -1,42 +1,82 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Obtendo os elementos do DOM
+    const meetingForm = document.getElementById('meeting-form');
+    const cancelFormButton = document.getElementById('cancel-form');
+    const consultFormButton = document.getElementById('consult-form');
+    const confirmLogoutButton = document.getElementById('confirm-logout');
+    const messageInput = document.getElementById('message-input');
 
-    document.getElementById('meeting-form').addEventListener('submit', function(event) {
-        event.preventDefault();
+    // Verificação para garantir que o formulário de reunião existe antes de adicionar o event listener
+    if (meetingForm) {
+        meetingForm.addEventListener('submit', function(event) {
+            event.preventDefault();
 
-        const date = document.getElementById('data').value;
-        const time = document.getElementById('horario').value;
-        const duration = document.getElementById('duracao').value;
-        const sector = document.getElementById('setor').value;
-        const speaker = document.getElementById('nome-orador').value;
-        const room = document.getElementById('sala').value;
-        const client = document.getElementById('cliente').value;
+            const date = document.getElementById('data').value;
+            const time = document.getElementById('horario').value;
+            const duration = document.getElementById('duracao').value;
+            const sector = document.getElementById('setor').value;
+            const speaker = document.getElementById('nome-orador').value;
+            const room = document.getElementById('sala').value;
+            const client = document.getElementById('cliente').value;
 
-        if (isPastTime(date, time)) {
-            alert("Não é possível agendar uma reunião para um horário que já passou.");
-            return;
-        }
+            if (isPastTime(date, time)) {
+                alert("Não é possível agendar uma reunião para um horário que já passou.");
+                return;
+            }
 
-        if (!validateInput(date, time, duration, sector, speaker, room, client)) {
-            alert("Por favor, preencha todos os campos corretamente.");
-            return;
-        }
+            if (!validateInput(date, time, duration, sector, speaker, room, client)) {
+                alert("Por favor, preencha todos os campos corretamente.");
+                return;
+            }
 
-        fetch('/agendar', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ date, time, duration, sector, speaker, room, client })
-        })
-        .then(response => response.json())
-        .then(result => {
-            alert(result.message);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Ocorreu um erro ao agendar a reunião. Por favor, tente novamente.');
+            fetch('/agendar', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ date, time, duration, sector, speaker, room, client })
+            })
+            .then(response => response.json())
+            .then(result => {
+                alert(result.message);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Ocorreu um erro ao agendar a reunião. Por favor, tente novamente.');
+            });
         });
-    });
+    }
+
+    // Verificando se o botão de cancelar existe antes de adicionar o event listener
+    if (cancelFormButton) {
+        cancelFormButton.addEventListener('click', function() {
+            toggleCancelForm();
+        });
+    }
+
+    // Verificando se o botão de consulta existe antes de adicionar o event listener
+    if (consultFormButton) {
+        consultFormButton.addEventListener('click', function() {
+            toggleConsultForm();
+        });
+    }
+
+    // Verificando se o input de mensagem existe antes de adicionar o event listener
+    if (messageInput) {
+        messageInput.addEventListener('input', function() {
+            adjustInputHeight();
+        });
+    }
+
+    // Verifica se o botão de logout existe e, se sim, adiciona o event listener
+    if (confirmLogoutButton) {
+        document.getElementById('logout-button').addEventListener('click', function(event) {
+            event.preventDefault();
+            confirmLogoutButton.classList.add('open');
+        });
+    }
+
+    // Funções auxiliares
 
     function isPastTime(date, time) {
         const now = new Date();
@@ -50,9 +90,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function toggleCancelForm() {
         const cancelForm = document.getElementById('cancel-form');
-        cancelForm.style.display = cancelForm.style.display === 'block' ? 'none' : 'block';
-        if (cancelForm.style.display === 'block') {
-            loadMeetings();
+        if (cancelForm) {
+            cancelForm.style.display = cancelForm.style.display === 'block' ? 'none' : 'block';
+            if (cancelForm.style.display === 'block') {
+                loadMeetings();
+            }
+        }
+    }
+
+    function toggleConsultForm() {
+        const consultForm = document.getElementById('consult-form');
+        if (consultForm) {
+            consultForm.style.display = consultForm.style.display === 'block' ? 'none' : 'block';
         }
     }
 
@@ -85,10 +134,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    function closeCancelForm() {
-        document.getElementById('cancel-form').style.display = 'none';
-    }
-
     function cancelMeeting(id) {
         fetch('/cancelar', {
             method: 'POST',
@@ -108,95 +153,10 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    function toggleConsultForm() {
-        const consultForm = document.getElementById('consult-form');
-        consultForm.style.display = consultForm.style.display === 'block' ? 'none' : 'block';
+    function adjustInputHeight() {
+        if (messageInput) {
+            messageInput.style.height = 'auto';
+            messageInput.style.height = (messageInput.scrollHeight) + 'px';
+        }
     }
-
-    function consultMeetings() {
-        const date = document.getElementById('consulta-data').value;
-        const client = document.getElementById('consulta-cliente').value;
-        const room = document.getElementById('consulta-sala').value;
-        const sector = document.getElementById('consulta-setor').value;
-
-        const params = new URLSearchParams({ date, client, room, sector });
-
-        fetch(`/consultar?${params.toString()}`)
-        .then(response => response.json())
-        .then(meetings => {
-            const results = document.getElementById('consult-results');
-            results.innerHTML = '';
-            meetings.forEach(meeting => {
-                const li = document.createElement('li');
-                li.textContent = `${meeting.date} - ${meeting.time} - ${meeting.duration} minutos - ${meeting.sector} - ${meeting.room} - ${meeting.client}`;
-                results.appendChild(li);
-            });
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Ocorreu um erro ao consultar as reuniões. Por favor, tente novamente.');
-        });
-    }
-
-    function downloadPDF() {
-        const { jsPDF } = window.jspdf;
-        const doc = new jsPDF();
-
-        const date = document.getElementById('consulta-data').value;
-        const client = document.getElementById('consulta-cliente').value;
-        const room = document.getElementById('consulta-sala').value;
-        const sector = document.getElementById('consulta-setor').value;
-
-        const params = new URLSearchParams({ date, client, room, sector });
-
-        fetch(`/consultar?${params.toString()}`)
-        .then(response => response.json())
-        .then(meetings => {
-            const tableColumn = ["DATA", "HORÁRIO", "TEMPO DE DURAÇÃO", "SETOR", "SALA", "CLIENTE"];
-            const tableRows = [];
-
-            meetings.forEach(meeting => {
-                const meetingData = [
-                    meeting.date,
-                    meeting.time,
-                    `${meeting.duration} minutos`,
-                    meeting.sector,
-                    meeting.room,
-                    meeting.client
-                ];
-                tableRows.push(meetingData);
-            });
-
-            doc.autoTable({
-                head: [tableColumn],
-                body: tableRows,
-                startY: 10,
-                theme: 'striped'
-            });
-
-            doc.save('reunioes.pdf');
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Ocorreu um erro ao gerar o PDF. Por favor, tente novamente.');
-        });
-    }
-
-    document.getElementById('logout-button').addEventListener('click', function(event) {
-        event.preventDefault();
-        document.getElementById('confirm-logout').classList.add('open');
-    });
-
-    var dialog = document.getElementById('confirm-logout');
-    var btnYes = dialog.querySelector('.yes');
-    var btnNo = dialog.querySelector('.no');
-
-    btnYes.addEventListener('click', function(event) {
-        var logoutHref = document.getElementById('logout-button').getAttribute('href');
-        window.location.href = logoutHref;
-    });
-
-    btnNo.addEventListener('click', function(event) {
-        dialog.classList.remove('open');
-    });
 });
